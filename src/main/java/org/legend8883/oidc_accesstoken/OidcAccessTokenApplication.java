57301edc0fe50fa94f8758a4a8457fc984e25e59
Bogdan <minecraftspeed3@gmail.com>
2026-05-06 14:51:43 +0300

    first commit

---
+++ src/main/java/org/legend8883/oidc_accesstoken/OidcAccessTokenApplication.java
@@ -0,0 +1,13 @@
+package org.legend8883.oidc_accesstoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcAccessTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OidcAccessTokenApplication.class, args);
+    }
+
+}
